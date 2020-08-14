54b18ec0eb62eb9bae5674dbaff411b0da692448
WidowMaker <littlegogacc2@gmail.com>
2020-08-14 15:07:10 +0530

    No Changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         Rootref = FirebaseDatabase.getInstance().getReference();
+       
 
 
 
